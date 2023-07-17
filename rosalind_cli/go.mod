b450eaa661fb7c482aa8c42535afdaabd5dcc034
mbeaver502 <jmbeaver1@gmail.com>
2023-07-17 13:55:59 -0500

    Scaffold argparse for CLI

--- rosalind_cli/go.mod
+++ rosalind_cli/go.mod
@@ -1,3 +1,5 @@
 module github.com/mbeaver502/rosalind_go/rosalind_cli
 
 go 1.20
+
+require github.com/akamensky/argparse v1.4.0
--- rosalind_cli/main.go
+++ rosalind_cli/main.go
@@ -1,24 +1,65 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/mbeaver502/rosalind_go/rosalind_lib/dna"
+	"github.com/akamensky/argparse"
 )
 
+const (
+	progName = "rosalind"
+	progDesc = "(Poor) Solutions to Rosalind exercises, written in Go."
+)
+
+var rosalindProblems = map[string]func(){
+	"dna":  ProblemDna,
+	"rna":  ProblemRna,
+	"revc": ProblemRevc,
+}
+
 func main() {
-	s := "AAAACCCGGT"
-	d, err := dna.New(s)
+	parser := argparse.NewParser(progName, progDesc)
+
+	problem := parser.String("p", "problem", &argparse.Options{
+		Required: true,
+		Validate: func(args []string) error {
+			for _, arg := range args {
+				if _, found := rosalindProblems[arg]; !found {
+					return errors.New("invalid problem")
+				}
+			}
+			return nil
+		},
+		Help:    `Specify the Rosalind problem in lowercase (identified at the end of the URL, e.g., https://rosalind.info/problems/dna/ => "dna")`,
+		Default: nil,
+	})
+
+	err := parser.Parse(os.Args)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(parser.Usage(err))
+		return
 	}
 
-	r, err := d.Transcribe()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	rosalindProblems[*problem]()
+}
 
-	log.Printf("dna: %s\n", d)
-	log.Printf("rna: %s\n", r)
-	log.Printf("rc:  %s\n", d.ReverseComplement())
+// TODO: Implement DNA problem
+// https://rosalind.info/problems/dna/
+func ProblemDna() {
+	log.Println("DNA problem")
+}
+
+// TODO: Implement RNA problem
+// https://rosalind.info/problems/rna/
+func ProblemRna() {
+	log.Println("RNA problem")
+}
+
+// TODO: Implement Revc problem
+// https://rosalind.info/problems/revc/
+func ProblemRevc() {
+	log.Println("Revc Problem")
 }
