9b9dfef459752775356ec9ddd12ecd96cf350e5a
mbeaver502 <jmbeaver1@gmail.com>
2023-07-17 14:34:31 -0500

    Add Transform method to nucleotide.Mapping

--- rosalind_lib/nucleotide/mapping.go
+++ rosalind_lib/nucleotide/mapping.go
@@ -17,3 +17,12 @@ func (nm Mapping) String() string {
 	}
 	return fmt.Sprintf("{%s}", strings.TrimRight(sb.String(), ", "))
 }
+
+// Transform will apply the mapping specified by
+// the Nucleotide Mapping nm on the Nucleotide nt.
+func (nm Mapping) Transform(nt Nucleotide) Nucleotide {
+	if transformed, found := nm[nt]; found {
+		return transformed
+	}
+	return nt
+}
