46166c871a3bba3b7467913f05cec02e13e3e65b
mbeaver502 <jmbeaver1@gmail.com>
2023-07-19 07:41:45 -0500

    Add missing comments on Fasta types and func

--- rosalind_lib/utils/fasta/fasta.go
+++ rosalind_lib/utils/fasta/fasta.go
@@ -7,13 +7,19 @@ import (
 	"strings"
 )
 
+// FastaEntry represents a FASTA-format entry.
+// The Label field may be a zero-length string.
 type FastaEntry struct {
 	Label   string
 	Content string
 }
 
+// FastaContent represents a slice of FastaEntry.
 type FastaContent []FastaEntry
 
+// ReadFastaContent reads the contents of the file f
+// into FastaContent. It is assumed that the content
+// of the file f is in correct FASTA format.
 func ReadFastaContent(f *os.File) FastaContent {
 	scanner := bufio.NewScanner(f)
 	labelRegex := regexp.MustCompile(`>Rosalind_\d{4}`) // Rosalind-specific label format
